6e4e1b7914818b3c0ff5abc7bc9a73a52f3b99c3
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-01-18 23:14:37 +0900

    Add SUIT font

---
+++ src/components/Common/GlobalStyle.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Global, css } from '@emotion/react';
+
+const defaultStyle = css`
+  @import url('http://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/static/woff2/SUIT.css');
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'SUIT', sans-serif;
+  }
+
+  html,
+  body,
+  #___gatsby {
+    height: 100%;
+  }
+
+  a,
+  a:hover {
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
+
+const GlobalStyle: React.FC = () => {
+  return <Global styles={defaultStyle} />;
+};
+
+export default GlobalStyle;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import GlobalStyle from 'components/Common/GlobalStyle';
+
+const IndexPage: React.FC = () => {
+  return (
+    <main>
+      <GlobalStyle />
+    </main>
+  );
+};
+
+export default IndexPage;
