96e29864781a76424117a78d0813f13dfd19d4cd
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-01-18 15:01:36 +0900

    Set typescript, eslint, prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,36 +3,27 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-plugin-typescript",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        isTSX: true,
+        allExtensions: true,
       },
     },
+    `gatsby-plugin-react-helmet`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `contents`,
+    //     path: `${__dirname}/contents`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
+    // To learn more, visit: <https://gatsby.dev/offline>
     // `gatsby-plugin-offline`,
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,25 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: <https://www.gatsbyjs.com/docs/node-apis/>
+ */
+
+// You can delete this file if you're not using it
+
+const path = require("path")
+
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        utils: path.resolve(__dirname, "src/utils"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
+    },
   })
 }
