53a9ef5c1b863f5ca35d650038021d5502d86aef
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-01-20 21:07:47 +0900

    Implement infinite scroll

--- src/components/Main/ArticleList.tsx
+++ src/components/Main/ArticleList.tsx
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled';
 import { Article } from './Article';
 import { ArticleType } from 'utils/Type';
-import { useMemo } from 'react';
+import useInfiniteScroll, {
+  useInfiniteScrollType,
+} from 'hooks/useInfiniteScroll';
 
 interface ArticleListProps {
   selectedCategory: string;
@@ -19,24 +21,12 @@ export const ArticleList: React.FC<ArticleListProps> = ({
   selectedCategory,
   articles,
 }) => {
-  const articleListData = useMemo(
-    () =>
-      articles.filter(
-        ({
-          node: {
-            frontmatter: { categories },
-          },
-        }: ArticleType) =>
-          selectedCategory !== '모두보기'
-            ? categories.includes(selectedCategory)
-            : true,
-      ),
-    [selectedCategory],
-  );
+  const { containerRef, articleList }: useInfiniteScrollType =
+    useInfiniteScroll(selectedCategory, articles);
 
   return (
-    <ArticleSection>
-      {articleListData.map(({ node: { id, frontmatter } }: ArticleType) => (
+    <ArticleSection ref={containerRef}>
+      {articleList.map(({ node: { id, frontmatter } }: ArticleType) => (
         <Article {...frontmatter} link="https://github.com" key={id} />
       ))}
     </ArticleSection>
---
+++ src/hooks/useInfiniteScroll.tsx
@@ -0,0 +1,68 @@
+import { MutableRefObject, useRef, useState, useMemo, useEffect } from 'react';
+import { ArticleType } from 'Utils/Type';
+
+export interface useInfiniteScrollType {
+  containerRef: MutableRefObject<HTMLDivElement | null>;
+  articleList: ArticleType[];
+}
+
+const NUMBER_OF_ITEMS_PER_PAGE = 10;
+
+const useInfiniteScroll = (
+  selectedCategory: string,
+  articles: ArticleType[],
+): useInfiniteScrollType => {
+  const containerRef: MutableRefObject<HTMLDivElement | null> =
+    useRef<HTMLDivElement>(null);
+
+  const [count, setCount] = useState<number>(1);
+
+  const articleListByCategory = useMemo<ArticleType[]>(
+    () =>
+      articles.filter(
+        ({
+          node: {
+            frontmatter: { categories },
+          },
+        }: ArticleType) =>
+          selectedCategory !== '모두보기'
+            ? categories.includes(selectedCategory)
+            : true,
+      ),
+    [selectedCategory],
+  );
+
+  const observer: IntersectionObserver = new IntersectionObserver(
+    (entries, observer) => {
+      if (!entries[0].isIntersecting) return;
+
+      setCount(value => value + 1);
+      observer.disconnect();
+    },
+  );
+
+  useEffect(() => setCount(1), [selectedCategory]);
+
+  useEffect(() => {
+    if (
+      NUMBER_OF_ITEMS_PER_PAGE * count >= articleListByCategory.length ||
+      containerRef.current === null ||
+      containerRef.current.children.length === 0
+    )
+      return;
+
+    observer.observe(
+      containerRef.current.children[containerRef.current.children.length - 1],
+    );
+  }, [count, selectedCategory]);
+
+  return {
+    containerRef,
+    articleList: articleListByCategory.slice(
+      0,
+      count * NUMBER_OF_ITEMS_PER_PAGE,
+    ),
+  };
+};
+
+export default useInfiniteScroll;
